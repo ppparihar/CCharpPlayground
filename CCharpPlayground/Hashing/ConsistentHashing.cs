d36b22f396003acb2049d74a13d2dca1a1aa37eb
Prem Parihar <prpariha@microsoft.com>
2023-10-11 12:19:53 +0530

    Consistent Hashing

---
+++ CCharpPlayground/Hashing/ConsistentHashing.cs
@@ -0,0 +1,71 @@
+﻿namespace CCharpPlayground.Hashing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class ConsistentHashing<T>
+    {
+        private readonly SortedDictionary<uint, T> circle = new SortedDictionary<uint, T>();
+        private readonly int virtualNodeCount;
+
+        public ConsistentHashing(int virtualNodeCount)
+        {
+            this.virtualNodeCount = virtualNodeCount;
+        }
+
+        public void AddNode(T node)
+        {
+            for (int i = 0; i < virtualNodeCount; i++)
+            {
+                uint hash = GetHash(node.ToString() + i);
+                circle[hash] = node;
+            }
+        }
+
+        public void RemoveNode(T node)
+        {
+            for (int i = 0; i < virtualNodeCount; i++)
+            {
+                uint hash = GetHash(node.ToString() + i);
+                circle.Remove(hash);
+            }
+        }
+
+        public T GetNode(string key)
+        {
+            if (circle.Count == 0)
+            {
+                throw new InvalidOperationException("No nodes available in the circle.");
+            }
+
+            uint hash = GetHash(key);
+
+            // Find the first node with a hash greater than or equal to the hash of the key.
+            foreach (var nodeHash in circle.Keys)
+            {
+                if (nodeHash >= hash)
+                {
+                    return circle[nodeHash];
+                }
+            }
+
+            // If no node with a hash greater than or equal to the key's hash is found,
+            // wrap around to the first node in the circle.
+            return circle.First().Value;
+        }
+
+        private uint GetHash(string input)
+        {
+            using (var sha = SHA256.Create())
+            {
+                byte[] data = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha.ComputeHash(data);
+                return BitConverter.ToUInt32(hashBytes, 0);
+            }
+        }
+    }
+
+}
---
+++ Playground.Test/ConsistentHashingTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Playground.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CCharpPlayground.Hashing;
+
+    public class ConsistentHashingTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var consistentHashing = new ConsistentHashing<string>(3);
+            consistentHashing.AddNode("A");
+            consistentHashing.AddNode("B");
+            consistentHashing.AddNode("C");
+
+            Assert.NotNull(consistentHashing.GetNode("1"));
+            Assert.NotNull(consistentHashing.GetNode("2"));
+            Assert.NotNull(consistentHashing.GetNode("3"));
+        }
+    }
+}
